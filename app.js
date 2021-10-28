d7c54e47f9aa472abab53171a18cb669d4d7b7dd
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-10-28 20:18:47 +0530

    feat: get from other file

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("start")
+const getText = document.querySelector("#getText");
+const output = document.querySelector("#output");
+const getUsers = document.querySelector("#get-users");
+
+function getTextFun() {
+    fetch("sample.txt")
+        .then((res) => res.text())
+        .then((data) => {
+            output.innerHTML = data;
+        })
+        .catch((err) => console.log(err))
+}
+
+function getUsersFun() {
+    fetch("users.json")
+        .then(res => res.json())
+        .then((data) => {
+            // console.log(data);
+            let resultOutput = `<h2>Users</h2>`;
+            data.forEach(function (user) {
+                resultOutput += `
+                    <ul>
+                    <li>ID: ${user.id}</li>
+                    <li>NAME: ${user.name}</li>
+                    <li>EMAIL: ${user.email}</li>
+                    </ul>
+                `;
+            });
+            output.innerHTML = resultOutput;
+
+        })
+}
+
+getUsers.addEventListener("click", getUsersFun)
+getText.addEventListener("click", getTextFun)
